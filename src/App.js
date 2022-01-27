3098812f600a35f242259a7514167963373174cf
Dhaval Gadhiya <dgadhiya@social27.com>
2022-01-27 18:55:15 +0530

    Working with various objects

--- src/App.js
+++ src/App.js
@@ -6,51 +6,96 @@ import theme from './theme';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-  
+
+import { useState } from "react";
+
 function App() {
+
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
   
         <CssBaseline />
-        <Container maxWidth="sm">
-          <Typography component="h1" variant="h1" align="center" 
-           color="textPrimary" gutterBottom>
-            Geeks for Geeks
-          </Typography>
-          <Typography variant="h2" align="center" 
-           color="textSecondary">
-            h2 Typography with secondary text colour
+
+        <Container maxWidth="md">
+
+          <Typography variant="h4" align="left" style={{marginTop: "50px"}} 
+           color="textPrimary">
+            ReactJS App
           </Typography>
           <br />
-          <Typography variant="h3" align="center" 
+
+          <Typography variant="h6" align="left" 
            color="textPrimary" paragraph>
-            h3 Typography variant with primary text colour
+            Working with various objects
           </Typography>
-          <br />
-          <Button variant="contained" 
-                  color="primary" > Primary 
-          </Button>
-          <Button variant="contained" 
-                  color="secondary"> Secondary
-          </Button>
-          {/* Since, color prop only takes primary and 
-          secondary as values,we define other colors 
-          in component's style */}
-          <Button variant="contained"
-          style={{background:theme.palette.error.main, margin: "0 10px"}}>
-          Error </Button>
-          <Button variant="contained" 
-          style={{background:theme.palette.warning.main}}>
-          Warning </Button>
-          <Button variant="contained" 
-          style={{background:theme.palette.info.main}}>
-          Info </Button>
-          <Button variant="contained" 
-          style={{background:theme.palette.success.main}}>
-          Success </Button>
-  
-          <br /><br />
+
+          <div style={{margin: "30px 0 35px"}}>
+          <Button variant="contained" color="primary"> Primary</Button>
+          <Button variant="contained" color="secondary"> Secondary</Button>
+
+            <Button variant="contained" color="secondary"
+            style={{background:theme.palette.error.main}}>
+            Error </Button>
+            <Button variant="contained" color="secondary"
+            style={{background:theme.palette.warning.main}}>
+            Warning </Button>
+            <Button variant="contained" color="secondary"
+            style={{background:theme.palette.info.main}}>
+            Info </Button>
+            <Button variant="contained" color="secondary"
+            style={{background:theme.palette.success.main}}>
+            Success </Button>
+          </div>
+
+          <form>
+            <div style={{marginBottom: "20px"}}>
+            <label>Name:<br />
+              <input
+                className='form-control'
+                type="text" 
+                placeholder="Enter Your Name"
+              />
+            </label>
+            </div>
+
+            <div style={{marginBottom: "20px"}}>
+              <label>Email:<br />
+                <input
+                  className='form-control'
+                  type="email" 
+                  placeholder="Enter Your Email"
+                />
+              </label>
+            </div>
+
+            <div style={{marginBottom: "20px"}}>
+              <label>Message:<br />
+                <textarea
+                  className='form-control'
+                  type="text" 
+                  placeholder="Enter Your Message"
+                />
+              </label>
+            </div>
+
+            <div style={{marginBottom: "20px"}}>
+              <label>Gender:</label>
+              <div style={{marginTop: "5px"}}>
+                <label style={{marginRight: "10px"}}>
+                  <input style={{verticalAlign: "text-top", marginLeft: "0"}} type="radio" value="Male" name="gender" /> Male
+                </label>
+                <label style={{marginRight: "10px"}}>
+                  <input style={{verticalAlign: "text-top"}} type="radio" value="Female" name="gender" /> Female
+                </label>
+                <label>
+                  <input style={{verticalAlign: "text-top"}} type="radio" value="Other" name="gender" /> Other
+                </label>     
+              </div>      
+            </div>
+
+          </form>
+
         </Container>
   
       </ThemeProvider>
