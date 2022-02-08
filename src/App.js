6afeb40835e2ff789cfae09fd444ff72481371cf
Dhaval Gadhiya <dgadhiya@social27.com>
2022-02-08 13:04:06 +0530

    Minor Change of UI

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-import { useState } from "react";
-
 function App() {
 
   return (
